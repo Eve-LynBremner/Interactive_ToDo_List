f47d50deb6db4612ad2ba38a379a04da7c87f897
Eve-LynBremner <emvanschoor@gmail.com>
2026-02-07 21:29:22 +0000

    comment out testing (console.logs), and edit comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
-/* using week 4's js example as a template and then tweaking it to suit my code */
+// using week 4's js example as a template and then tweaking it to suit my code 
 
+// creating an array to store the to do list items, so that I can perform error handling
 itemList = [];
-// add event listener to the Add button, and call the onClickAdd function
 document.getElementById("addButton").addEventListener("click", onClickAdd);
 
 function onClickAdd() {
-   // retrieves input
   var toDoItem = document.getElementById("toDoItem");
 
   // trim the value before inserting
@@ -23,27 +22,24 @@ function onClickAdd() {
     return;
   }
 
-  // add item to array, trim and make lower case so that all items are comparable
+  // add item to array, make lower case so that all items are comparable
   itemList.push(toDoItemValue.toLowerCase());
 
-  // retrieves unordered list
   var toDoContent = document.getElementById("todo");
   
-  // creates list element
   var newLiEl = document.createElement("li");
 
-  // creates delete button
+  // create delete button
   var deleteButton = document.createElement("button");
   deleteButton.textContent = "delete";
-  // adds an id to the button so we can later identify it
   deleteButton.id = "deleteButton";
 
-  // creates complete button
+  // create complete button
   var completeButton = document.createElement("button");
   completeButton.textContent = "complete";
   completeButton.id = "completeButton";
 
-  // creates complete button
+  // create edit button
   var editButton = document.createElement("button");
   editButton.textContent = "edit";
   editButton.id = "editButton";
@@ -57,21 +53,12 @@ function onClickAdd() {
   // console.log(newLiEl);
 
   toDoItem.value = "" ;
-  console.log(itemList);
+  // console.log(itemList);
 }
 
-// Note to self:
-// cannot add event listener to the delete and complete buttons because these buttons and the list items do not yet exist
-// instead add event listener to the unordered list as this exists from the start
-// event --> an object automatically created by the browser when something happens, like a click
-// function(event) --> the function takes the event object and can return info about the event
-// event.target --> is the item that was clicked
-// event.target.id = "deleteButton" --> checks if the item that was clicked has id = "deleteButton".
-// If wanted to search by class and not id then would do the below
-// event.target.classList.contains("deleteButton") --> checks if the item that was clicked has the class deleteButton
-// event.type --> what kind of event it was
-// event.target.parentElement.remove() --> deletes the parent element of the item that was clicked,
-// so it removes the relevant (to do) list item from the page, as the button is the child of said (to do) list element
+/* cannot add event listener to the delete and complete buttons because these buttons and the list items do not yet exist.
+   Instead add event listener to the unordered list as this exists from the start. Then check when an event happens and if that 
+   event is a button click. Then perform one of the 3 functions below depending on which button was clicked. */
 
 document.getElementById("todo").addEventListener("click", function(event){
   // console.log(event.type);
@@ -85,11 +72,11 @@ document.getElementById("todo").addEventListener("click", function(event){
 
       // remove item from to do list
       event.target.parentElement.remove();
-      console.log(itemList);
+      // console.log(itemList);
   } 
   else if (event.target.id === "completeButton") {
-    // retrieve the to do list item so that we can insert it into the completed list. 
-    // Need "firstChild" otherwise it also returns the text from the button
+    /* get the to do list item so that we can insert it into the completed list. 
+       Need "firstChild" otherwise it also returns the text from the button */
     var toDoItemValue = event.target.parentElement.firstChild.textContent;  
     // console.log(event.target.parentElement.firstChild.textContent)
 
@@ -103,7 +90,7 @@ document.getElementById("todo").addEventListener("click", function(event){
 
     // remove item from to do list
     event.target.parentElement.remove();
-    console.log(itemList);
+    // console.log(itemList);
   }
     else if (event.target.id === "editButton") {
     currentItem = event.target.parentElement.firstChild.textContent;
@@ -115,7 +102,7 @@ document.getElementById("todo").addEventListener("click", function(event){
       return;
     }
 
-    // check if user enters item already in the list and
+    // check if user enters item already in the list
     if(itemList.includes(newItem.trim().toLowerCase())){
       alert("Item already in list.")
       return;
@@ -127,10 +114,19 @@ document.getElementById("todo").addEventListener("click", function(event){
     // apply edit
     event.target.parentElement.firstChild.textContent = newItem.trim();
 
-    // add edit to array
+    // add new item to array
     itemList.push(newItem.trim().toLowerCase());
-    console.log(itemList);
+    // console.log(itemList);
   }
 });
 
- 
+// Note to self:
+// event --> an object automatically created by the browser when something happens, like a click
+// function(event) --> the function takes the event object and can return info about the event
+// event.target --> is the item that was clicked
+// event.target.id = "deleteButton" --> checks if the item that was clicked has id = "deleteButton".
+// If wanted to search by class and not id then would do the below
+// event.target.classList.contains("deleteButton") --> checks if the item that was clicked has the class deleteButton
+// event.type --> what kind of event it was
+// event.target.parentElement.remove() --> deletes the parent element of the item that was clicked,
+// so it removes the relevant (to do) list item from the page, as the button is the child of said (to do) list element
