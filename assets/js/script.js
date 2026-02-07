4170cd694c1bcf72d19ac954b12aca3608af05af
Eve-LynBremner <emvanschoor@gmail.com>
2026-02-07 02:21:20 +0000

    added array for error handling of duplicates, and added edit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 /* using week 4's js example as a template and then tweaking it to suit my code */
 
-
+itemList = [];
 // add event listener to the Add button, and call the onClickAdd function
 document.getElementById("addButton").addEventListener("click", onClickAdd);
 
@@ -8,12 +8,24 @@ function onClickAdd() {
    // retrieves input
   var toDoItem = document.getElementById("toDoItem");
 
-  // error handling
-  if(toDoItem.value === ""){
+  // trim the value before inserting
+  var toDoItemValue = toDoItem.value.trim();
+
+  // check value not blank
+  if(toDoItemValue === ""){
     alert("This cannot be blank. Please add item.")
     return;
   }
  
+  // check value not already in list
+  if(itemList.includes(toDoItemValue.toLowerCase())){
+    alert("Item already in list.")
+    return;
+  }
+
+  // add item to array, trim and make lower case so that all items are comparable
+  itemList.push(toDoItemValue.toLowerCase());
+
   // retrieves unordered list
   var toDoContent = document.getElementById("todo");
   
@@ -31,10 +43,16 @@ function onClickAdd() {
   completeButton.textContent = "complete";
   completeButton.id = "completeButton";
 
+  // creates complete button
+  var editButton = document.createElement("button");
+  editButton.textContent = "edit";
+  editButton.id = "editButton";
+
   // adds the list item and buttons to html
-  newLiEl.textContent = toDoItem.value;
-  newLiEl.appendChild(deleteButton);
+  newLiEl.textContent = toDoItemValue;
   newLiEl.appendChild(completeButton);
+  newLiEl.appendChild(editButton);
+  newLiEl.appendChild(deleteButton);
   toDoContent.appendChild(newLiEl);
   // console.log(newLiEl);
 
@@ -54,25 +72,55 @@ function onClickAdd() {
 // event.target.parentElement.remove() --> deletes the parent element of the item that was clicked,
 // so it removes the relevant (to do) list item from the page, as the button is the child of said (to do) list element
 
-document.getElementById("todo").addEventListener("click", function(event) {
+document.getElementById("todo").addEventListener("click", function(event){
   // console.log(event.type);
   // console.log(event.target);
   // console.log(event.target.parentElement);
   
   if (event.target.id === "deleteButton") {
       event.target.parentElement.remove();
+
+      // remove item from array
+      var toDoItemValue = event.target.parentElement.firstChild.textContent;  
+      itemList.pop(toDoItemValue.toLowerCase());
   } 
   else if (event.target.id === "completeButton") {
-    // retrieve the to do list item so that we can insert it into the completed list. Need "firstChild" otherwise it also returns the text from the button
-    var toDoItem = event.target.parentElement.firstChild.textContent;  
+    // retrieve the to do list item so that we can insert it into the completed list. 
+    // Need "firstChild" otherwise it also returns the text from the button
+    var toDoItemValue = event.target.parentElement.firstChild.textContent;  
     // console.log(event.target.parentElement.firstChild.textContent)
+
+    // remove item from array
+    itemList.pop(toDoItemValue.toLowerCase());
 
     var completedList = document.getElementById("completed");
     var newLiEl = document.createElement("li");
-    newLiEl.textContent = toDoItem;
+    newLiEl.textContent = toDoItemValue;
     completedList.appendChild(newLiEl);
 
     // remove item from to do list
     event.target.parentElement.remove();
   }
+    else if (event.target.id === "editButton") {
+    currentItem = event.target.parentElement.firstChild.textContent;
+    //
+    newItem = prompt("Edit your item:", currentItem);
+
+    // if user does not enter anything then exit without changes
+    if (!newItem || newItem.trim() === "") {
+      return;
+    }
+    
+    // need to remove edited item from array 
+    itemList.pop(event.target.parentElement.firstChild.textContent.toLowerCase());
+
+    // apply edit
+    event.target.parentElement.firstChild.textContent = newItem.trim();
+
+    // add edit to array
+    itemList.push(newItem.trim().toLowerCase());
+
+  }
 });
+
+ 
